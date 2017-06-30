436ecaf2117d994363ebcbb4e5465ad62c7e4ade
Edmond Lai <kml42638@co.champaign.il.us>
2017-06-30 10:37:43 -0500

    added temp database

--- setUpDB.py
+++ setUpDB.py
@@ -2,7 +2,10 @@ import arcpy
 import os
 import shutil
 import tempfile
-
+from cuuats.datamodel.fields import BaseField, OIDField, GeometryField, \
+    StringField, NumericField, ScaleField, MethodField, WeightsField
+from cuuats.datamodel.features import BaseFeature
+from cuuats.datamodel.workspaces import WorkspaceManager
 
 def setUpModule():
     WorkspaceFixture.setUpModule()
@@ -13,6 +16,7 @@ def tearDownModule():
 class WorkspaceFixture(object):
     PCD_NAME = 'pdc.gdb'
     CCGISC_NAME = 'ccgisc.gdb'
+    TEMP_NAME = 'temp.gdb'
     FEATURE_CLASS_NAME = 'Road'
     FEATURE_CLASS_TYPE = 'POLYLINE'
     FEATURE_CLASS_FIELDS = (
@@ -28,16 +32,17 @@ class WorkspaceFixture(object):
     )
     CCGISC_FEATURE_CLASS_DATA = (
         ('Road A', '2', 35, 2, ([0, 0], [0, 6])),
-        ('B Avenue', '3', 40, 4, ([0, 0], [5, 3])),
+        ('B Avenue', '3', 40, 4, ([0, 0], [5, 0])),
         ('C Street', '4', 25, 1, ([0, 0], [0, -5]))
     )
 
     @classmethod
     def setUpModule(cls):
-        # Create a new backup file geodatabase
+        # Create the path for geodatabase
         cls.workspace_dir = tempfile.mkdtemp()
         cls.PCD_path = os.path.join(cls.workspace_dir, cls.PCD_NAME)
         cls.CCGISC_path = os.path.join(cls.workspace_dir, cls.CCGISC_NAME)
+        cls.TEMP_path = os.path.join(cls.workspace_dir, cls.TEMP_NAME)
 
         # Create file geodatabase
         arcpy.CreateFileGDB_management(cls.workspace_dir, cls.PCD_NAME)
@@ -71,6 +76,12 @@ class WorkspaceFixture(object):
             for row in cls.CCGISC_FEATURE_CLASS_DATA:
                 cursor.insertRow(row)
 
+    @classmethod
+    def setUpTempDatabase(cls):
+        arcpy.CreateFileGDB_management(cls.workspace_dir, cls.TEMP_NAME)
+        cls.TEMP_FC_PATH = os.path.join(cls.workspace_dir, cls.TEMP_NAME, cls.FEATURE_CLASS_NAME)
+        cls.CCGIS_FC_PATH = os.path.join(cls.CCGISC_path, cls.FEATURE_CLASS_NAME)
+        arcpy.Copy_management(cls.CCGIS_FC_PATH, cls.TEMP_FC_PATH)
 
     @classmethod
     def tearDownModule(cls):
