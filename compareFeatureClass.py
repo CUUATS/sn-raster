a88f56af3070222ef54125a42ddebf576a3b28c4
Edmond Lai <kml42638@co.champaign.il.us>
2017-07-05 09:41:53 -0500

    Added copy feature class to pcd db

--- compareFeatureClass.py
+++ compareFeatureClass.py
@@ -1,7 +1,7 @@
 from setUpDB import WorkspaceFixture
 import arcpy
 import os
-import datetime
+from datetime import date
 
 WorkspaceFixture.setUpModule()
 WorkspaceFixture.setUpTempDatabase()
@@ -16,36 +16,44 @@ class compare_FeatureClass(object):
     FEATURE_CLASS_PCD = os.path.join(GDB_PATH_1, FEATURE_CLASS_NAME_1)
     FEATURE_CLASS_TEMP = os.path.join(GDB_PATH_2, FEATURE_CLASS_NAME_2)
 
+    print(GDB_PATH_2)
+
 
     @classmethod
-    def add_notfication_field(self):
-    # add the require field to the target and original database
+    def addNotificationField(self):
+    # Add the require field to the target and original database
         pcd_list = ["Match", "Del", "Added"]
         for field in pcd_list:
             arcpy.AddField_management(self.FEATURE_CLASS_PCD, field, "TEXT")
-        arcpy.AddField_management(self.FEATURE_CLASS_TEMP, "Match", "TEXT")
+        temp_list = ["Match", "Added"]
+        for field in temp_list:
+            arcpy.AddField_management(self.FEATURE_CLASS_TEMP, field, "TEXT")
+
 
     @classmethod
-    # read the feature class from both database and compare the shape, update if matches are found
-    def compare_FeatureClassShape(cls):
+    # Read the feature class from both database and compare the shape, update if matches are found
+    def compareFeatureClassShape(cls):
         fields = ["SHAPE", "Match"]
+        match = 0
         with arcpy.da.UpdateCursor(cls.FEATURE_CLASS_PCD, fields) as cursor1:
             for row1 in cursor1:
                 hash1 = hash(row1[0])
-                print(hash1)
                 with arcpy.da.UpdateCursor(cls.FEATURE_CLASS_TEMP, fields) as cursor2:
                     for row2 in cursor2:
                         hash2 = hash(row2[0])
-                        print(hash2)
                         if hash1 == hash2:
-                            print("match")
+                            match = match + 1
                             row1[1] = "Yes"
                             row2[1] = "Yes"
                             cursor1.updateRow(row1)
                             cursor2.updateRow(row2)
 
+        print("There are {} matches".format(match))
+
+
     @classmethod
-    def delete_PCD_fc(cls):
+    # Mark PCD fc class for deletion
+    def deletePCDFeatureClass(cls):
         fields = ["Match", "Del"]
         with arcpy.da.UpdateCursor(cls.FEATURE_CLASS_PCD, fields) as cursor:
             for row in cursor:
@@ -53,23 +61,35 @@ class compare_FeatureClass(object):
                     row[1] = "Yes"
                     cursor.updateRow(row)
 
+
     @classmethod
-    def add_temp_fc(cls):
+    # Mark new features with current date
+    def addTempFeatureClass(cls):
         field = ["Match", "Added"]
-        with arcpy.da.UpdateCursor(cls.FEATURE_CLASS_TEMP, "*") as cursor:
-            with arcpy.da.UpdateCursor(cls.FEATURE_CLASS_PCD, "*") as cursor1:
-                for row in cursor:
-                    if row["Match"] != "Yes":
-                        row["Added"] = "Today"
-                        cursor.updateRow(row)
-                        cursor1.insertRow(row)
+        with arcpy.da.UpdateCursor(cls.FEATURE_CLASS_TEMP, field) as cursor_temp:
+            for row in cursor_temp:
+                if row[0] != "Yes":
+                    row[1] = date.today().strftime('%Y%m%d')
+                    cursor_temp.updateRow(row)
 
 
+    @classmethod
+    # Append new features into the PCD database
+    def appendFeature(cls):
+        arcpy.MakeFeatureLayer_management(cls.FEATURE_CLASS_TEMP, 'temp_layer')
+        arcpy.SelectLayerByAttribute_management('temp_layer', "NEW_SELECTION", "Added IS NOT NULL")
+        arcpy.Append_management('temp_layer', cls.FEATURE_CLASS_PCD, "NO_TEST")
+        arcpy.SelectLayerByAttribute_management('temp_layer', "CLEAR_SELECTION")
+        arcpy.Delete_management('temp_layer')
+
+def main():
+    compare_FeatureClass.addNotificationField()
+    compare_FeatureClass.compareFeatureClassShape()
+    compare_FeatureClass.deletePCDFeatureClass()
+    compare_FeatureClass.addTempFeatureClass()
+    compare_FeatureClass.appendFeature()
+    #WorkspaceFixture.tearDownModule()
 
 
-compare_FeatureClass.add_notfication_field()
-compare_FeatureClass.compare_FeatureClassShape()
-compare_FeatureClass.delete_PCD_fc()
-compare_FeatureClass.add_temp_fc()
-
-#WorkspaceFixture.tearDownModule()
+if __name__ == "__main__":
+    main()
